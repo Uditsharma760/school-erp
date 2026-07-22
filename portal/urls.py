f0d791fd845f5022ca7be455245d225792f5b818
Uditsharma760 <uditsharma760@gmail.com>
2026-07-23 01:06:06 +0530

    Update urls.py

--- portal/urls.py
+++ portal/urls.py
@@ -1,8 +1,41 @@
-from django.urls import path
-
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
 from . import views
 
 urlpatterns = [
+    path(
+    "password/forgot/",
+    auth_views.PasswordResetView.as_view(
+        template_name="registration/password_reset_form.html",
+        email_template_name="registration/password_reset_email.txt",
+        subject_template_name="registration/password_reset_subject.txt",
+        success_url=reverse_lazy("password_reset_done"),
+    ),
+    name="password_reset",
+),
+path(
+    "password/forgot/done/",
+    auth_views.PasswordResetDoneView.as_view(
+        template_name="registration/password_reset_done.html",
+    ),
+    name="password_reset_done",
+),
+path(
+    "password/reset/<uidb64>/<token>/",
+    auth_views.PasswordResetConfirmView.as_view(
+        template_name="registration/password_reset_confirm.html",
+        success_url=reverse_lazy("password_reset_complete"),
+    ),
+    name="password_reset_confirm",
+),
+path(
+    "password/reset/complete/",
+    auth_views.PasswordResetCompleteView.as_view(
+        template_name="registration/password_reset_complete.html",
+    ),
+    name="password_reset_complete",
+),
     path("", views.home, name="home"),
     path("login/", views.login_view, name="login"),
     path("management/login/", views.management_login, name="management_login"),
